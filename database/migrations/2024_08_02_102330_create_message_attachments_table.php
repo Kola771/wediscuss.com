e4010e56a9de07a6818fb49d1b13f89aa3fb5f20
Kola771 <koladeaboudou@gmail.com>
2024-08-02 13:04:46 +0200

    migration pièces jointes créée

--- database/migrations/2024_08_02_102330_create_message_attachments_table.php
+++ database/migrations/2024_08_02_102330_create_message_attachments_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('message_attachments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("message_id")->constrained("messages");
+            $table->string("name", 255); // dsgfg3dfgf2dgf.jpg
+            $table->string("path", 1024); // sdfsdfsd4sdfsd4fsd4fsd4f.jpg
+            $table->string("mime", 255); // jpg, png, xsl
+            $table->integer("size"); // taille du fichier
             $table->timestamps();
         });
     }
