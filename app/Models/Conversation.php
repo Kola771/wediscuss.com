8b315485c3db645f76c798e1ea294ba49ff6d383
Kola771 <koladeaboudou@gmail.com>
2024-08-09 16:31:50 +0200

    récupération des données pour la sidebar

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -88,4 +88,21 @@ class Conversation extends Model
         return $this->belongsTo(User::class, 'user_id2');
     }
 
+    /**
+     * Fonction qui récupère toutes les conversations associées à l'utilisateur connecté
+     */
+
+    public static function getConversationForSidebar(User $user)
+    {
+        // récupérer tous les utilisateurs autre que celui connecté
+        $users = User::getUsersExcept($user);
+        // récupérer tous les groupes auxquels l'utilisateur connecé appartient
+        $groups = Group::getGroupsExcept($user);
+        // renvoyer toutes les conversations que l'utilisateur a effectué avec les utilisateurs et les groupes
+        return $users->map(function(User $user) {
+            return $user->toConversationArray();
+        })->concat($groups->map(function (Group $group) {
+            return $group->toConversationArray();
+        }));
+    }
 }
--- app/Models/Group.php
+++ app/Models/Group.php
@@ -94,4 +94,43 @@ class Group extends Model
         // Établit la relation avec le modèle User pour identifier le propriétaire du groupe
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * Récupère les groupes auxquels l'utilisateur connecté appartient et les messages associés
+     * @param \App\Models\User $user
+     * @return \Illuminate\Support\Collection
+     */
+    public static function getGroupsExcept(User $user) {
+        $userId = $user->id;
+
+        $query = self::select(["groups.*", "messages.message as last_message", 
+        "messages.created_at as last_message_date"])
+        ->join("group_user", "group_user.group_id", "=", "groups.id")
+        ->leftJoin("messages", "messages.id", "=", "groups.last_message_id")
+        ->where("group_user.user_id", $userId)
+        ->orderByDesc("messages.created_at")
+        ->orderBy("groups.name");
+
+        return $query->get();
+    }
+
+    /**
+     * Méthode qui transforme le model en tableau
+     * @return array
+     */
+    public function toConversationArray() {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "description" => $this->description,
+            "is_group" => true,
+            "owner_id" => $this->owner_id,
+            "users" => $this->users,
+            "user_ids" => $this->users->pluck("id"),
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+            "last_message" => $this->last_message,
+            "last_message_date" => $this->last_message_date,
+        ];
+    }
 }
