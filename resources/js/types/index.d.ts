a7cdf1f80b7aeac2cbdde709bb4d57808cc6738d
Kola771 <koladeaboudou@gmail.com>
2024-08-09 11:57:06 +0200

    canal de présence mis en place

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -3,6 +3,11 @@ export interface User {
   name: string;
   email: string;
   email_verified_at: string;
+  avatar: string;
+  is_admin: boolean;
+  created_at: string;
+  updated_at: string;
+  blocked_at: string;
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
