28b7f02bb347791d3b2c85e21dae1fd1d1c501cf
Kola771 <koladeaboudou@gmail.com>
2024-08-10 10:31:52 +0200

    formatage

--- resources/js/Layouts/ChatLayout.vue
+++ resources/js/Layouts/ChatLayout.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="flex overflow-hidden flex-1 h-full">
     <!-- Sidebar -->
-    <div class="transition-all w-full sm:w-[220px] md:w-[320px] bg-accent 
-    flex flex-col">
+    <div class="transition-all w-full sm:w-[220px] md:w-[320px] bg-accent flex flex-col">
       <!-- En tête fixe -->
       <div class="flex flex-col">
-        <div class="flex items-center justify-between py-2 px-3 font-medium
-        sticky top-0 z-10">
+        <div class="flex items-center justify-between py-2 px-3 font-medium sticky top-0 z-10">
           Conversations
           <button title="Créer un groupe" class="text-icon hover:text-icon-hover">
             <Icon class="h-5 w-5" icon="heroicons:pencil-square" />
@@ -18,21 +16,20 @@
       </div>
 
       <!-- Liste des conversations -->
-      <div class="flex-1 scrollbar-thin scrollbar-thumb-slate-700 
-      scrollbar-track-slate-300 overflow-y-auto px-3 h-full">
-          
-          <!-- Itme de conversation pour chaque conversation -->
-          <ConversationItem 
-          v-for="conversation in filterConversations" 
+      <div
+        class="flex-1 scrollbar-thin scrollbar-thumb-slate-700 scrollbar-track-slate-300 overflow-y-auto px-3 h-full"
+      >
+        <!-- Itme de conversation pour chaque conversation -->
+        <ConversationItem
+          v-for="conversation in filterConversations"
           :key="conversation.is_group ? `group_${conversation.id}` : `ùser_${conversation.id}`"
-          :conversation="conversation" 
-          />
+          :conversation="conversation"
+        />
       </div>
     </div>
 
     <!-- Zone d'affichage des messages -->
-    <div class="flex-1">Messages
-    </div>
+    <div class="flex-1">Messages</div>
   </div>
 </template>
 
@@ -43,22 +40,21 @@ import TextInput from '@/Components/TextInput.vue';
 import { Conversation, User } from '@/types';
 import { usePage } from '@inertiajs/vue3';
 import { computed, onBeforeUnmount, onMounted, ref } from 'vue';
-import { Icon } from "@iconify/vue";
+import { Icon } from '@iconify/vue';
 
 // Données partagées
 const page = usePage();
 
 // Données réactives de gestion des conversations
 const conversations = ref<Conversation[]>(page.props.conversations);
-const localConversations = computed<Conversation[]>(
-  () => conversations.value);
-  
-const search = ref<string>("");
+const localConversations = computed<Conversation[]>(() => conversations.value);
+
+const search = ref<string>('');
 
 const sortedConversations = computed<Conversation[]>(() => {
   return localConversations.value.sort((a, b) => {
     // on recherche les personnes bloquées
-    if(a.blocked_at && b.blocked_at) {
+    if (a.blocked_at && b.blocked_at) {
       return a.blocked_at > b.blocked_at ? 1 : -1;
     } else if (a.blocked_at) {
       return 1;
@@ -67,23 +63,25 @@ const sortedConversations = computed<Conversation[]>(() => {
     }
 
     // on recherche en fonction de la date du message
-    if(a.last_message_date && b.last_message_date) {
+    if (a.last_message_date && b.last_message_date) {
       return b.last_message_date.localeCompare(a.last_message_date);
-    } else if(a.last_message_date) {
+    } else if (a.last_message_date) {
       return 1;
     } else {
       return 0;
     }
   });
-})
+});
 
 const filterConversations = computed<Conversation[]>(() => {
   return sortedConversations.value.filter((conversation) => {
     const searchTerm = search.value.toLocaleLowerCase();
-    return conversation.name.toLocaleLowerCase().includes(searchTerm) ||
-    conversation.email?.toLocaleLowerCase().includes(searchTerm)
-  })
-})
+    return (
+      conversation.name.toLocaleLowerCase().includes(searchTerm) ||
+      conversation.email?.toLocaleLowerCase().includes(searchTerm)
+    );
+  });
+});
 // Utilisateurs connectés
 const onlineUsersObj = ref<Record<string, User>>({});
 
